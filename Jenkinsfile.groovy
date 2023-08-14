684e5f5afe92ab7805c171c473b09454e77c2880
Yevhen Duma <yevhen.duma@gmail.com>
2023-08-14 15:33:52 +0300

    chore: update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,7 @@ grafanaPipeline {
     buildParameters = ' --platform linux/amd64 \
     --build-arg BINGO=false \
 	--build-arg WIRE_TAGS="oss" \
-	--build-arg COMMIT_SHA=$$(git rev-parse --short HEAD) \
-	--build-arg BUILD_BRANCH=$$(git rev-parse --abbrev-ref HEAD)'
+    '
+	// --build-arg COMMIT_SHA=$$(git rev-parse --short HEAD) \
+	// --build-arg BUILD_BRANCH=$$(git rev-parse --abbrev-ref HEAD)'
 }
